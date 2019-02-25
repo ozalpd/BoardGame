76efdae3035d4ee1a48e091ab77b5fd0766b90b6
Özalp Döndüren <ozalp@donduren.com>
2019-02-25 13:06:03 +0300

    first commit

---
+++ Assets/Scripts/BoardController.cs
@@ -0,0 +1,148 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class BoardController : MonoBehaviour
+{
+    public GameObject tilePrefab;
+    public Material selectMaterial;
+
+    public GameObject[] playPieces;
+    public Quaternion[] PieceRotations { get; private set; }
+
+
+    [Range(2, 100)]
+    [Tooltip("Number of columns")]
+    public int cols = 8;
+
+    [Range(2, 100)]
+    [Tooltip("Number of rows")]
+    public int rows = 8;
+
+    [Range(2, 4)]
+    public int players = 2;
+    private int turn = 0;
+
+    private float posX;
+    private float posZ;
+    private List<TileController> tiles;
+
+    public TileController SelectedTile
+    {
+        get { return _selTile; }
+        set
+        {
+            if (_selTile != null && tileMat != null)
+            {
+                _selTile.GetComponent<Renderer>().material = tileMat;
+            }
+            _selTile = value;
+            if (_selTile != null)
+            {
+                tileMat = _selTile.GetComponent<Renderer>().material;
+                _selTile.GetComponent<Renderer>().material = selectMaterial;
+            }
+            else
+            {
+                tileMat = null;
+            }
+        }
+    }
+    private TileController _selTile;
+    private Material tileMat;
+
+    private void Awake()
+    {
+        var boardGO = new GameObject("Board");
+        tiles = new List<TileController>();
+        posX = -0.5f * ((float)cols - 1);
+        posZ = -0.5f * ((float)rows - 1);
+
+        PieceRotations = new Quaternion[playPieces.Length];
+        float[] rotY = new float[] { 45f, 225f, 135f, 315f };
+
+        for (int i = 0; i < playPieces.Length; i++)
+        {
+            PieceRotations[i] = Quaternion.Euler(0, rotY[i % rotY.Length], 0);
+        }
+
+        for (int c = 0; c < cols; c++)
+        {
+            for (int r = 0; r < rows; r++)
+            {
+                var go = Instantiate(tilePrefab, new Vector3(posX + c, 0f, posZ + r), Quaternion.identity);
+                go.transform.SetParent(boardGO.transform);
+
+                var tile = go.GetComponent<TileController>();
+                tile.Board = this;
+                tile.ColNr = c;
+                tile.RowNr = r;
+                if (c == 0 && r == 0)
+                {
+                    tile.PieceNr = 0;
+                }
+                else if (c == cols - 1 && r == rows - 1)
+                {
+                    tile.PieceNr = 1;
+                }
+                else if (c == 0 && r == rows - 1 && players > 2)
+                {
+                    tile.PieceNr = 2;
+                }
+                else if (c == cols - 1 && r == 0 && players > 3)
+                {
+                    tile.PieceNr = 3;
+                }
+
+                tiles.Add(tile);
+            }
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray.origin, ray.direction, out hitInfo))
+            {
+                var tile = hitInfo.collider.GetComponent<TileController>();
+                if (tile != null)
+                {
+                    if (tile.PieceNr == turn)
+                    {
+                        SelectedTile = tile;
+                    }
+                    else if (SelectedTile != null && !tile.HasPiece
+                         && (tile.CanJump(SelectedTile) || tile.IsNeighbour(SelectedTile)))
+                    {
+                        tile.PieceNr = SelectedTile.PieceNr;
+                        if (tile.IsNeighbour(SelectedTile))
+                        {
+                            foreach (var t in tiles)
+                            {
+                                if (t.PieceNr != tile.PieceNr && t.IsNeighbour(tile))
+                                    t.PieceNr = SelectedTile.PieceNr;
+                            }
+                        }
+                        SelectedTile = null;
+                        turn = (turn + 1) % players;
+                        DisplayScores();
+                    }
+                }
+            }
+        }
+    }
+    RaycastHit hitInfo = new RaycastHit();
+    Ray ray = new Ray();
+
+    public void DisplayScores()
+    {
+        for (int i = 0; i < players; i++)
+        {
+            int score = tiles.Count(t => t.PieceNr == i);
+            Debug.Log("Player" + (i + 1) + ": " + score);
+        }
+    }
+}
---
+++ Assets/Scripts/TileController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileController : MonoBehaviour
+{
+    public string Address
+    {
+        get
+        {
+            return string.Format("{0}x{1}", ColNr, RowNr);
+        }
+    }
+
+    public BoardController Board { get; set; }
+    public int ColNr { get; set; }
+    public int RowNr { get; set; }
+
+    public bool HasPiece { get { return _pieceNr.HasValue; } }
+
+    public int PieceNr
+    {
+        get { return _pieceNr ?? -1; }
+        set
+        {
+            if (value >= 0 && value < Board.playPieces.Length)
+            {
+                _pieceNr = value;
+                if (playerGo != null)
+                    Destroy(playerGo);
+
+                playerGo = Instantiate(Board.playPieces[value],
+                                transform.position,
+                                Board.PieceRotations[value]);
+                playerGo.transform.SetParent(transform);
+            }
+        }
+    }
+    private int? _pieceNr;
+    private GameObject playerGo;
+
+    public bool CanJump(TileController tile)
+    {
+        int colDiff = Mathf.Abs(ColNr - tile.ColNr);
+        int rowDiff = Mathf.Abs(RowNr - tile.RowNr);
+
+        return colDiff < 3 && rowDiff < 3;
+    }
+
+    public bool IsNeighbour(TileController tile)
+    {
+        int colDiff = Mathf.Abs(ColNr - tile.ColNr);
+        int rowDiff = Mathf.Abs(RowNr - tile.RowNr);
+
+        return colDiff < 2 && rowDiff < 2;
+    }
+}
